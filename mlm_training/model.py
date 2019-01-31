568a97f9434464a3aa2c211944422af84b1dc95e
adrienycart <adrien.ycart@gmail.com>
2019-01-31 20:02:21 +0900

    remove print

--- mlm_training/model.py
+++ mlm_training/model.py
@@ -468,7 +468,6 @@ class Model:
                 for var_to_init in variables_to_initialize:
                     if var_to_init.decode("utf-8")  in var.name:
                         var_list += [var]
-            print(var_list)
             init = tf.variables_initializer(var_list)
             sess.run(init)
 
