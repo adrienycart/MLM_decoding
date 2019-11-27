f122a793c395a8336b9d1863b4d5dcf53f429b60
adrienycart <adrien.ycart@gmail.com>
2019-11-27 20:48:03 +0000

    add option to synthesise MIDI files

--- evaluate_beats.py
+++ evaluate_beats.py
@@ -120,8 +120,9 @@ parser.add_argument('--load',action='store_true',help="use pre-computed values (
 parser.add_argument('--save',type=str,help="save GT and estimated beats as CSV files in specified folder ('same' saves in input_folder)")
 parser.add_argument("--max_len",type=str,help="test on the first max_len seconds of each text file. Anything other than a number will evaluate on whole files. Default is 30s.",
                     default=30)
+parser.add_argument('--midi',action='store_true',help="synthesize MIDI file instead of loading wav files")
 parser.add_argument('--subbeats',action='store_true',help='also compute subbeat subdivisions')
-parser.add_argument('--file',type=str,help="play one specific file only (or any file containing the string)")
+parser.add_argument('--file',type=str,help="analyse one specific file only (or any file containing the string)")
 parser.add_argument('--play_GT', action='store_true',help="play audio with ground-truth beat and downbeat positions (and subbeats if --subbeat is used)")
 parser.add_argument('--play_estim',action='store_true',help="play audio with estimated beat and downbeat positions (and subbeats if --subbeat is used)" )
 
@@ -168,11 +169,15 @@ for fn in os.listdir(input_folder):
 
 
             # Estimate beat positions
-            sig_proc = madmom.audio.signal.SignalProcessor(sample_rate=44100, num_channels=1, start=0, stop=max_len,dtype=np.float32)
-            sig = sig_proc(filename_input)
+            if args.midi:
+                sig = midi_data.synthesize()
+                sig = sig[:int(max_len*44100)]
+            else:
+                sig_proc = madmom.audio.signal.SignalProcessor(sample_rate=44100, num_channels=1, start=0, stop=max_len,dtype=np.float32)
+                sig = sig_proc(filename_input)
 
-            fs = sig.sample_rate
-            dur = sig.length
+                fs = sig.sample_rate
+                dur = sig.length
 
             if args.play_GT and SOUND:
                 sig_mix_ratio = 0.7
@@ -232,5 +237,5 @@ for fn in os.listdir(input_folder):
                 play_sound(audio)
 
 print(f"Average beat F-measure: {sum(all_Fs)/len(all_Fs)}")
-if self.subbeats:
+if args.subbeats:
     print(f"Average sub_beat F-measure: {sum(all_Fs_sub)/len(all_Fs_sub)}")
