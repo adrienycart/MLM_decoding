8f3a4e54cec8ba73ea33cba93d3dbfc394b3849a
adrienycart <adrien.ycart@gmail.com>
2020-02-29 22:30:28 +0000

    add code to generate directly the latex table based on result dictionnaries

--- plot_figures.py
+++ plot_figures.py
@@ -5,16 +5,28 @@ import os
 import scipy.stats
 
 
+
+
+
+
+
 # folder_1 = 'results/quant/save-quant'
 # folder_1 = "results/save-gtweight/save-quant-gtweight"
 # folder_1 = "results/quant/sched0.7_w0.8_k50_b200_h12_new"
 # folder_1 = "results/quant/w0.8_k40_b100_h20"
-folder_1 = "results/results-20/save/wm-quant-sched"
-folder_2 = 'results/results-20/save/wm-time-sched'
-folder_BL = 'results/results-20/save/baseline-quant'
+# folder_1 = "results/results_pkl/beat/est_prior"
+# folder_2 = "results/results_pkl/beat/est_prior_sched"
+folder_1 = 'results/results-20/save/wm-time-sched'
+folder_2 = 'results/results-20/save/wm-quant'
+# folder_BL = 'results/results-20/save/baseline-quant'
 # folder_2 = 'results/baseline/quant'
 # folder_2 = "results/quant/sched0.7_w0.8_k50_b200_h12"
 # folder_2 = "results/save-hmm/save-quant-hmm"
+
+# folder_1 = "results/results_pkl/beat/baseline_est"
+# folder_2 = 'results/results_pkl/beat/est_prior_sched'
+folder_BL = 'results/results-20/save/baseline-quant'
+
 
 print(folder_1)
 print(folder_2)
@@ -24,14 +36,14 @@ try :
     results_1 = pickle.load(open(os.path.join(folder_1,'results.pkl'), "rb"))
 except:
     results_1 = pickle.load(open(os.path.join(folder_1,'results.p'), "rb"))
-try:
-    results_2 = pickle.load(open(os.path.join(folder_2,'results_quant.pkl'), "rb"))
-except:
-    results_2 = pickle.load(open(os.path.join(folder_2,'results_quant.p'), "rb"))
-# try :
-#     results_2 = pickle.load(open(os.path.join(folder_2,'results.pkl'), "rb"))
+# try:
+#     results_2 = pickle.load(open(os.path.join(folder_2,'results_quant.pkl'), "rb"))
 # except:
-#     results_2 = pickle.load(open(os.path.join(folder_2,'results.p'), "rb"))
+#     results_2 = pickle.load(open(os.path.join(folder_2,'results_quant.p'), "rb"))
+try :
+    results_2 = pickle.load(open(os.path.join(folder_2,'results.pkl'), "rb"))
+except:
+    results_2 = pickle.load(open(os.path.join(folder_2,'results.p'), "rb"))
 try:
     results_BL = pickle.load(open(os.path.join(folder_BL,'results.pkl'), "rb"))
 except:
@@ -43,8 +55,9 @@ keys = list(results_BL.keys())
 print(len(keys))
 F_fs = []
 F_ns = []
-for key in keys:
+for key in sorted(keys):
     try:
+        # print(key, results_1[key][0][2])
         F_fs += [[results_1[key][0][2],results_2[key][0][2],results_BL[key][0][2]]]
         F_ns += [[results_1[key][1][2],results_2[key][1][2],results_BL[key][1][2]]]
     except KeyError:
@@ -172,3 +185,111 @@ print("Notes: ",scipy.stats.ttest_rel(F_ns[:,1],F_ns[:,0]))
 #                             # f"Note: R value = {r_value_n:.3f}, P value = {p_value_n:.3f}, Standard error = {std_err_n:.3f}")
 # fig.tight_layout()
 # plt.show()
+
+
+#### GET ALL RESULTS:
+
+# all_folders_gt = ['results/results_thesis/beat_gt/baseline/results_gap.p',
+#                'results/results_thesis/beat_gt/hmm/results_gap.p',
+#                'results/results_thesis/beat_gt/cw/results_gap.p',
+#                'results/results_thesis/beat_gt/cw_sched/results_gap.p',
+#                'results/results_thesis/beat_gt/wm/results_gap.p',
+#                'results/results_thesis/beat_gt/wm_sched/results_gap.p',
+#                'results/results_thesis/beat_gt/pm/results_gap.p',
+#                'results/results_thesis/beat_gt/pm_sched/results_gap.p',]
+#
+# all_folders_est = ['results/results_thesis/beat_est/baseline/results_gap.p',
+#                'results/results_thesis/beat_est/hmm/results_gap.p',
+#                'results/results_thesis/beat_est/cw/results_gap.p',
+#                'results/results_thesis/beat_est/cw_sched/results_gap.p',
+#                'results/results_thesis/beat_est/wm/results_gap.p',
+#                'results/results_thesis/beat_est/wm_sched/results_gap.p',
+#                'results/results_thesis/beat_est/pm/results_gap.p',
+#                'results/results_thesis/beat_est/pm_sched/results_gap.p',]
+#
+# # all_folders_gt = ['results/results_thesis/time/baseline/results_gap.p',
+# #                'results/results_thesis/time/hmm/results_gap.p',
+# #                'results/results_thesis/time/cw/results_gap.p',
+# #                'results/results_thesis/time/cw_sched/results_gap.p',
+# #                'results/results_thesis/time/wm/results_gap.p',
+# #                'results/results_thesis/time/wm_sched/results_gap.p',
+# #                'results/results_thesis/time/pm/results_gap.p',
+# #                'results/results_thesis/time/pm_sched/results_gap.p',]
+# #
+# # all_folders_est = ['results/results_thesis/quant/baseline/results_gap.p',
+# #                'results/results_thesis/quant/hmm/results_gap.p',
+# #                'results/results_thesis/quant/cw/results_gap.p',
+# #                'results/results_thesis/quant/cw_sched/results_gap.p',
+# #                'results/results_thesis/quant/wm/results_gap.p',
+# #                'results/results_thesis/quant/wm_sched/results_gap.p',
+# #                'results/results_thesis/quant/pm/results_gap.p',
+# #                'results/results_thesis/quant/pm_sched/results_gap.p',]
+#
+# results_array_gt = np.zeros([len(all_folders_gt),6])
+#
+# for i,folder in enumerate(all_folders_gt):
+#     # try :
+#     #     results = pickle.load(open(os.path.join(folder,'results.pkl'), "rb"))
+#     # except:
+#     #     results = pickle.load(open(os.path.join(folder,'results.p'), "rb"))
+#     results = pickle.load(open(folder, "rb"))
+#
+#     note = []
+#     frame = []
+#     for key in keys:
+#         try:
+#             frame += [results[key][0]]
+#             note += [results[key][1]]
+#         except KeyError:
+#             print(key)
+#             keys.remove(key)
+#
+#     results_array_gt[i,:3]=np.mean(frame,axis=0)
+#     results_array_gt[i,3:]=np.mean(note,axis=0)
+#
+# results_array_est = np.zeros([len(all_folders_est),6])
+#
+# for i,folder in enumerate(all_folders_est):
+#     # try :
+#     #     results = pickle.load(open(os.path.join(folder,'results.pkl'), "rb"))
+#     # except:
+#     #     results = pickle.load(open(os.path.join(folder,'results.p'), "rb"))
+#     results = pickle.load(open(folder, "rb"))
+#
+#     note = []
+#     frame = []
+#     for key in keys:
+#         try:
+#             frame += [results[key][0]]
+#             note += [results[key][1]]
+#         except KeyError:
+#             print(key)
+#             keys.remove(key)
+#
+#     results_array_est[i,:3]=np.mean(frame,axis=0)
+#     results_array_est[i,3:]=np.mean(note,axis=0)
+#
+# results_array = np.concatenate([results_array_gt,results_array_est],axis=1)
+#
+# str_array = np.zeros_like(results_array,dtype=object)
+# for i,j in np.ndindex(results_array.shape):
+#     str_array[i,j]=f"{results_array[i,j]*100:.1f}"
+#
+# # print(str_array)
+#
+# max_idx_1 = np.argmax(results_array[:,:6],axis=0)
+# max_idx_2 = np.argmax(results_array[:,6:],axis=0)
+#
+# # print( max_idx_1, max_idx_2)
+#
+# for j, idx in enumerate(max_idx_1):
+#     str_array[idx,j]='\\textbf{'+str_array[idx,j]+'}'
+# for j, idx in enumerate(max_idx_2):
+#     str_array[idx,6+j]='\\textbf{'+str_array[idx,6+j]+'}'
+#
+# # print(str_array)
+#
+# labels = ['Kelz',"HMM","CW",'CW+S',"WM",'WM+S','PM','PM+S']
+#
+# for i,row in enumerate(str_array):
+#     print(' & '.join([labels[i]]+list(row))+' \\\\')
